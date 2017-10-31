9be0121fdc4cf3cb0a2163de833d9e5cc2932aa3
Ujjawal Garg <ujjawal.garg@sjsu.edu>
2017-10-31 15:58:30 -0700

    Lab 11 Part 1 & 2

---
+++ lab11/lab11.js
@@ -0,0 +1,54 @@
+//JSLint Options - "in development", "for statement", "white mess", "this"
+var name = "Monty";
+function Rabbit(name) {
+    "use strict";
+    this.name = name;
+}
+var r = new Rabbit("Python");
+console.log(r.name);  // ERROR!!!
+console.log(name);    // Prints "Python"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//JSLint Options - "in development", "for statement", "white mess"
+function swap(arr, i, j) {
+  "use strict";
+  var tmp = arr[i]; 
+  arr[i] = arr[j]; 
+  arr[j] = tmp;
+}
+function sortAndGetLargest (arr) {
+  "use strict";
+  var tmp = arr[0]; // largest elem
+  var i = 0;
+  var j = 0;
+  for (i=0; i<arr.length; i += 1) {
+    if (arr[i] > tmp) {
+     tmp = arr[i];
+    }
+    for (j=i+1; j<arr.length; j+=1) {
+      if (arr[i] < arr[j]) {
+        swap(arr,i,j);
+      }
+    }
+  }
+  return tmp;
+}
+var largest = sortAndGetLargest([99,2,43,8,0,21,12]);
+console.log(largest); // should be 99, but prints 0
+
+
+
+
+
